8ea6b31ccd77c2c138f939a27a3d5440de348163
alic <alicvictor11@gmail.com>
2024-08-18 22:08:44 -0300

    feat: add Empresa class with attributes and necessary relationships

---
+++ src/main/java/br/edu/ifpb/pweb2/Jar/model/Empresa.java
@@ -0,0 +1,53 @@
+package br.edu.ifpb.pweb2.Jar.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Empresa implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cnpj;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private String telefoneContato;
+
+    @Column(nullable = false, unique = true)
+    private String emailContato;
+
+    @Column(nullable = false)
+    private String pessoaContato;
+
+    @Column(nullable = false)
+    private String atividadePrincipal;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Lob
+    @Column(nullable = false)
+    private byte[] documentoComprovacaoEndereco;
+
+    @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Oferta> ofertas = new HashSet<>();
+}
