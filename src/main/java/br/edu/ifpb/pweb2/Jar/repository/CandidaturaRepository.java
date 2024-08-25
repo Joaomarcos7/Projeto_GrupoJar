232876c1f01c069d8f8507c0a88b550ef991b52a
Ricardo Luiz <105940717+RicardoLuiz05@users.noreply.github.com>
2024-08-25 01:57:54 -0300

    feat: Implementação da filtragem de alunos ainda não aprovados

--- src/main/java/br/edu/ifpb/pweb2/Jar/repository/CandidaturaRepository.java
+++ src/main/java/br/edu/ifpb/pweb2/Jar/repository/CandidaturaRepository.java
@@ -2,6 +2,9 @@ package br.edu.ifpb.pweb2.Jar.repository;
 
 import br.edu.ifpb.pweb2.Jar.model.Aluno;
 import br.edu.ifpb.pweb2.Jar.model.Candidatura;
+import br.edu.ifpb.pweb2.Jar.model.EstadoCandidatura;
+import br.edu.ifpb.pweb2.Jar.model.OfertaEstagio;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +14,8 @@ import java.util.List;
 public interface CandidaturaRepository extends JpaRepository<Candidatura, Long> {
 
     List<Candidatura> findByAluno(Aluno aluno);
+    List<Candidatura> findByOfertaEstagio(OfertaEstagio ofertaEstagio);
+    List<Candidatura> findByEstado(EstadoCandidatura estado);
 
     boolean existsByAlunoIdAndOfertaEstagioId(Long alunoId, Long ofertaId);
 }
--- src/main/java/br/edu/ifpb/pweb2/Jar/service/CandidaturaService.java
+++ src/main/java/br/edu/ifpb/pweb2/Jar/service/CandidaturaService.java
@@ -2,12 +2,15 @@ package br.edu.ifpb.pweb2.Jar.service;
 
 import br.edu.ifpb.pweb2.Jar.model.Aluno;
 import br.edu.ifpb.pweb2.Jar.model.Candidatura;
+import br.edu.ifpb.pweb2.Jar.model.EstadoCandidatura;
 import br.edu.ifpb.pweb2.Jar.model.OfertaEstagio;
 import br.edu.ifpb.pweb2.Jar.repository.CandidaturaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CandidaturaService {
@@ -18,10 +21,21 @@ public class CandidaturaService {
     public void save(Candidatura candidatura) {
         candidaturaRepository.save(candidatura);
     }
-
+    
     public List<Candidatura> buscarPorAluno(Aluno aluno) { return candidaturaRepository.findByAluno(aluno); }
+    
 
     public boolean existsByAlunoIdAndOfertaId(Long alunoId, Long ofertaId) {
         return candidaturaRepository.existsByAlunoIdAndOfertaEstagioId(alunoId, ofertaId);
     }
+    
+    public List<Aluno> buscarAlunosNaoSelecionados() {
+        return candidaturaRepository.findByEstado(EstadoCandidatura.PENDENTE)
+            .stream()
+            .map(Candidatura::getAluno)
+            .collect(Collectors.toList());
+    }
+
+
+    
 }
