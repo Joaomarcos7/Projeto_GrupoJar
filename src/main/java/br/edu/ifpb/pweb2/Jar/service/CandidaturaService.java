981700684292448487f49dd6bdf4b88abb76dfc7
alic <alicvictor11@gmail.com>
2024-08-25 12:52:29 -0300

    feat: coordenador ve ficha do candidato

--- src/main/java/br/edu/ifpb/pweb2/Jar/service/CandidaturaService.java
+++ src/main/java/br/edu/ifpb/pweb2/Jar/service/CandidaturaService.java
@@ -3,14 +3,12 @@ package br.edu.ifpb.pweb2.Jar.service;
 import br.edu.ifpb.pweb2.Jar.model.Aluno;
 import br.edu.ifpb.pweb2.Jar.model.Candidatura;
 import br.edu.ifpb.pweb2.Jar.model.EstadoCandidatura;
-import br.edu.ifpb.pweb2.Jar.model.OfertaEstagio;
 import br.edu.ifpb.pweb2.Jar.repository.CandidaturaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class CandidaturaService {
@@ -21,19 +19,17 @@ public class CandidaturaService {
     public void save(Candidatura candidatura) {
         candidaturaRepository.save(candidatura);
     }
+
+    public Optional<Candidatura> findById(Long id) { return candidaturaRepository.findById(id); }
     
     public List<Candidatura> buscarPorAluno(Aluno aluno) { return candidaturaRepository.findByAluno(aluno); }
-    
 
     public boolean existsByAlunoIdAndOfertaId(Long alunoId, Long ofertaId) {
         return candidaturaRepository.existsByAlunoIdAndOfertaEstagioId(alunoId, ofertaId);
     }
     
-    public List<Aluno> buscarAlunosNaoSelecionados() {
-        return candidaturaRepository.findByEstado(EstadoCandidatura.PENDENTE)
-            .stream()
-            .map(Candidatura::getAluno)
-            .collect(Collectors.toList());
+    public List<Candidatura> buscarPorAlunosNaoSelecionados() {
+        return candidaturaRepository.findByEstado(EstadoCandidatura.PENDENTE);
     }
 
 
