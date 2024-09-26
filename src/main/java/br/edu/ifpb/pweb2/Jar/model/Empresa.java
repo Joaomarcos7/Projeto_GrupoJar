b341df2a7132840cf6b1be98c9fcda2f5f7f3cda
Juliana Ferreira <julianafercavalcante@hotmail.com>
2024-09-25 22:55:09 -0300

    feat: adição dos validators de empresa

--- src/main/java/br/edu/ifpb/pweb2/Jar/model/Empresa.java
+++ src/main/java/br/edu/ifpb/pweb2/Jar/model/Empresa.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.pweb2.Jar.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,27 +22,35 @@ public class Empresa extends Usuario implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @NotBlank(message = "O nome da empresa é obrigatório.")
     @Column(nullable = false)
     private String nome;
 
+    @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter 14 dígitos.")
     @Column(nullable = false, unique = true)
     private String cnpj;
 
+    @NotBlank(message = "O endereço é obrigatório.")
     @Column(nullable = false)
     private String endereco;
 
+    @Pattern(regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}", message = "O telefone deve estar no formato (XX) XXXXX-XXXX.")
     @Column(nullable = false)
     private String telefoneContato;
 
+    @NotBlank(message = "O nome da pessoa de contato é obrigatório.")
     @Column(nullable = false)
     private String pessoaContato;
 
+    @NotBlank(message = "A atividade principal é obrigatória.")
     @Column(nullable = false)
     private String atividadePrincipal;
 
+    @NotNull(message = "A URL é obrigatória.")
     @Column(nullable = false)
     private URL url;
 
+    @NotNull(message = "O documento de comprovação de endereço é obrigatório.")
     @Column(nullable = false)
     private byte[] documentoComprovacaoEndereco;
 
